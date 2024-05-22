a03e4efa6af73fb24e25a93a70e2a12bc1172573
matheusgnreis <matheus@e-com.club>
2024-05-22 18:47:17 -0300

    chore: update storefront layout

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,4 @@
 // Add your custom JavaScript for storefront pages here.
+window.$ecomConfig.set('default_img_size', 'zoom')
+const nameIf = window.ecomPassport.customer.display_name ? window.ecomPassport.customer.display_name : ' entre ou cadastre-se'
+document.getElementById('nameIf').innerHTML = 'Olá ' + nameIf
